fcf5614777d3652ff52a899742a510ab457ba037
Sy Tran <sy.tran@unified.com>
2019-10-22 17:21:44 +0700

    Can stream from webcam

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, send_from_directory
 import cv2
 import argparse
 import threading, os
@@ -16,6 +16,19 @@ wvs = None
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/hls")
+def hls():
+    return render_template("hls.html")
+
+@app.route("/stream/<path:path>")
+def stream(path):
+    cache = None if ".m3u8" not in path else 0
+    return send_from_directory('out', path, cache_timeout=cache)
+
+@app.route("/js/<path:path>")
+def public(path):
+    return send_from_directory('js', path)
 
 def generate():
     global wvs
